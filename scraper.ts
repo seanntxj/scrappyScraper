1ee5ce10ccb0b48e38a9f26aa74bd205a0789c9a
Sean Tan <seanntxj@gmail.com>
2023-08-18 11:23:39 +0800

    General scraper

---
+++ scraper.ts
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+
+/**
+ * Scrapes a webpage for all listings from the link given
+ * @param link A filled search link for a product. Highly encouraged to add sort to recent for best results.
+ * Example: https://www.carousell.com.my/search/iPhone%2014%20Pro?addRecent=false&canChangeKeyword=false&includeSuggestions=false&searchId=2Ehwhj&sort_by=3
+ * @param containerForListings The HTML element which contains a SINGLE listing.
+ * Example for Carousell & Mudah: 'div[data-testid^="listing"]'
+ * @returns All listing items seen from the search result
+ */
+const scraper = async (
+  link: string,
+  containerForListings: string
+): Promise<Array<string>> => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto(link);
+  await page.waitForSelector(containerForListings, {
+    timeout: 30_000,
+  });
+
+  // For every instance of the div found with the listing data-testid tag, do this
+  // Every returned object is then concatinated into an array
+  const cards: Array<string> = await page.$$eval(
+    containerForListings,
+    (listingsHTML: any[]) => {
+      return listingsHTML.map((listingHTML) => {
+        return listingHTML.innerText
+          .trim()
+          .split("\n")
+          .filter((item: string) => item.trim() !== "");
+      });
+    }
+  );
+
+  await browser.close();
+  return cards;
+};
+
+export default scraper;
--- test.ts
+++ test.ts
@@ -1,45 +1,9 @@
-import { Listing } from "./types";
-import puppeteer from "puppeteer";
-
+import scraper from "./scraper";
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
-  );
-  await page.goto("https://www.mudah.my/malaysia/for-sale?q=Samsung");
-
-  const cards = await page.$$eval(
-    'div[data-qa-locator^="general"]',
-    (listingsHTML) => {
-      return listingsHTML.map((listingHTML) => {
-        const isFirstItemListingName = (item: string) => {
-          return !(parseInt(item, 10).toString() === item);
-        };
-
-        const convertPriceToFloat = (paragraphContent: string | null) => {
-          if (paragraphContent) {
-            return parseFloat(paragraphContent.replace(/\D/g, ""));
-          }
-          return 0;
-        };
-
-        const combinedListingDetails = listingHTML.innerText.trim().split("\n");
-        // const listing: Listing = {
-        //   name: isFirstItemListingName(combinedListingDetails[0])
-        //     ? combinedListingDetails[0]
-        //     : combinedListingDetails[1],
-        //   price: isFirstItemListingName(combinedListingDetails[0])
-        //     ? convertPriceToFloat(combinedListingDetails[1])
-        //     : convertPriceToFloat(combinedListingDetails[2]),
-        //   location: combinedListingDetails[combinedListingDetails.length - 1],
-        // };
-        return combinedListingDetails;
-      });
-    }
+  const res = await scraper(
+    "https://www.carousell.com.my/search/iPhone 14 Pro?addRecent=false&canChangeKeyword=false&includeSuggestions=false&sort_by=3",
+    'div[data-testid^="listing"]'
   );
 
-  console.log(cards);
-
-  await browser.close();
+  console.log(res);
 })();
