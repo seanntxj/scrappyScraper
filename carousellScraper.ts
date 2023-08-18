45ec06dbaf956793fbb303f970ea557ab84c84ae
Sean Tan <seanntxj@gmail.com>
2023-08-18 11:14:41 +0800

    Comments added

--- carousellScraper.ts
+++ carousellScraper.ts
@@ -1,6 +1,12 @@
 const puppeteer = require("puppeteer");
 import { Listing } from "./types";
 
+/**
+ * Scrapes Carousell webpage for all listings from the link given
+ * @param link Carousell filled search link for a product. Highly encouraged to add sort to recent for best results.
+ * Example: https://www.carousell.com.my/search/iPhone%2014%20Pro?addRecent=false&canChangeKeyword=false&includeSuggestions=false&searchId=2Ehwhj&sort_by=3
+ * @returns All listing items seen from the search result
+ */
 const carousellScraper = async (link: string): Promise<Array<Listing>> => {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
@@ -10,6 +16,8 @@ const carousellScraper = async (link: string): Promise<Array<Listing>> => {
     timeout: 30_000,
   });
 
+  // For every instance of the div found with the listing data-testid tag, do this
+  // Every returned object is then concatinated into an array
   const cards: Array<Listing> = await page.$$eval(
     listingsHTMLContainer,
     (listingsHTML: any[]) => {
@@ -58,8 +66,8 @@ const carousellScraper = async (link: string): Promise<Array<Listing>> => {
 
         const listing: Listing = {
           name: thirdItemIsCarousellProtect
-            ? listingDetailsArr[3] ?? "Err"
-            : listingDetailsArr[2] ?? "Err",
+            ? listingDetailsArr[3]
+            : listingDetailsArr[2],
           price: thirdItemIsCarousellProtect
             ? convertPriceToFloat(listingDetailsArr[4])
             : convertPriceToFloat(listingDetailsArr[3]),
