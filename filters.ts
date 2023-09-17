e2c5328344359ab613e21385ad7fa16b040dd260
seanntxj <seanntxj@gmail.com>
2023-09-18 00:57:03 +0800

    Improved filters - Filter by elapsed time - Fixed blacklist filter being too strict (subtext matching changed to full-text)

--- filters.ts
+++ filters.ts
@@ -29,6 +29,16 @@ export const filterByPrice = (minPrice: number = 0, maxPrice: number = Infinity)
 }
 
 /**
+ * Checks if a listing is within a posted within a certain recentness
+ * @param listing The Listing object
+ * @param timeSincePostedSeconds The maximum amount of time since the listing of posted
+ * @returns 
+ */
+export const filterByElaspedTime = (timeSincePostedSeconds: number = Infinity): FilterFunction => {
+  return (listing: Listing) => listing.price <= timeSincePostedSeconds;
+}
+
+/**
  * Checks if a listing's title has or doesn't have certain keywords (case insensitive)
  * @param listing The Listing object
  * @param mustInclude An array of strings that MUST be in a listing's title (recommended to be the same as your search term)
@@ -37,15 +47,31 @@ export const filterByPrice = (minPrice: number = 0, maxPrice: number = Infinity)
  */
 export const filterByKeywords = (mustInclude: string[], mustExclude: string[]): FilterFunction => {
   return (listing: Listing) => {
+    
+    // Check if the listing's title does not contain any of the "mustExclude" keywords
+    // Perform a whole-word match
+    function containsBlacklistedWord(blacklist: string[], givenString: string): boolean {
+      // Split the given string into words using whitespace as the separator
+      const words = givenString.split(/\s+/);
+    
+      // Iterate through each word in the blacklist
+      for (const blacklistedWord of blacklist) {
+        // Check if the blacklistedWord is included in the words array as a whole word
+        if (words.includes(blacklistedWord)) {
+          return true; // Found a blacklisted word
+        }
+      }
+    
+      return false; // None of the blacklisted words were found
+    }
+
     const title = listing.name.toLowerCase(); // Convert title to lowercase for case-insensitive comparison
 
     // Check if the listing's title contains all the "mustInclude" keywords
+    // Substring match
     const includesAllKeywords = mustInclude.every(keyword => title.includes(keyword.toLowerCase()));
 
-    // Check if the listing's title does not contain any of the "mustExclude" keywords
-    const excludesNoKeywords = mustExclude.every(keyword => !title.includes(keyword.toLowerCase()));
-
     // Return true if the listing meets both conditions
-    return includesAllKeywords && excludesNoKeywords;
+    return includesAllKeywords && !containsBlacklistedWord(mustExclude, title);
   };
 }
